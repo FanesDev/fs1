355af95dc65ddc136acb49e34f95f91137fafacc
Marcus Imbiriba <marcusimbiriba@fanesdev.com.br>
2016-11-18 14:07:15 -0300

    Alteração de página inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    FanesDev
+                    TechnoArla
                 </div>
 
                 <div class="links">
